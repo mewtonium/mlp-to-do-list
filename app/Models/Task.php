d6408629bf5a9b7cf527209ba9f1944d7dae8fdf
Mark Wilkinson <56736714+mewtonium@users.noreply.github.com>
2025-08-19 12:27:51 +0100

    Added task model, migration and factory

---
+++ app/Models/Task.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /** @use HasFactory<\Database\Factories\TaskFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'completed_at',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @return array
+     */
+    protected function casts(): array
+    {
+        return [
+            'completed_at' => 'datetime',
+        ];
+    }
+}
---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(rand(min: 3, max: 7)),
+        ];
+    }
+
+    /**
+     * Indicate that the task has been completed.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function completed()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'completed_at' => now(),
+            ];
+        });
+    }
+}
