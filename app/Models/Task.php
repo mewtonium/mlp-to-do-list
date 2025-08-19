8fa3bd175f919e19027c41e5aee409568b648a96
Mark Wilkinson <56736714+mewtonium@users.noreply.github.com>
2025-08-19 15:40:11 +0100

    Added `completed` task query scope

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
@@ -31,5 +31,13 @@ class Task extends Model
         return [
             'completed_at' => 'datetime',
         ];
+    }
+
+    /**
+     * Scope a query to only include completed tasks.
+     */
+    protected function scopeCompleted(Builder $query): void
+    {
+        $query->whereNotNull('completed_at');
     }
 }
